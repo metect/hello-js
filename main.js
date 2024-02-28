97c650afcf5d99c2d6a01e690c04a1fd9cdf69fc
amirali irvany <dev.amirali.irvany@gmail.com>
2024-02-28 14:27:33 +0330

    Update main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+
+var first_name = 'amirali'
+var last_name = 'irvany';
+
+var language = 'JavaScript';
+
+
+const say_hello = function (message) {
+    return 'Hello ' + message
+}
+
+console.log(say_hello(language));
+console.log('Codeing by ' + first_name + ' ' + last_name);
